219972649bd1135a82554d78db36184cf71730d0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-25 14:50:35 +0300

    Add API endpoints for users

---
+++ api/User.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Register a new user
+router.post('/register', (req, res) => {
+
+})
+
+// Login a user
+router.post('/login', (req, res) => {
+
+})
