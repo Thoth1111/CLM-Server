ffe22830bc471ba2ddef8793bf85796669d3959d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-25 13:50:52 +0300

    Setup entry point and connection to database

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-require('./config/db');
+require ('dotenv').config();
+const express  = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port  = process.env.PORT || 8000;
 
-const app = require('express')();
-const port = process.env.PORT || 3000;
+const uri = process.env.MONGODB_URI;
+console.log(uri);
 
-const bodyParser = require('express');
-app.use(bodyParser());
+async function connect() {
+    try {
+        await mongoose.connect(uri)
+        console.log('Connected to the database');
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        await mongoose.connection.close();
+    }
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+connect().catch(console.error);
+
+app.listen(8000, ()=> {
+    console.log(`Server started on port ${port}`);
 });
