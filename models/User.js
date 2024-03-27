71c3cd027dc2d2fb9e4b936b04ef0ae5b782b811
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-27 18:04:33 +0300

    Update User model

--- models/User.js
+++ models/User.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    national_id_number: {type: String, required: true},
     phone_number: {type: String, required: true},
+    password: {type: String, required: true},
 });
 
 const User = mongoose.model('User', UserSchema);
