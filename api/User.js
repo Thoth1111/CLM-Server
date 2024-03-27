af968d7fd63d1e28e48ac6eb5e8eaa5f33b271f7
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-27 18:05:06 +0300

    Update User API

--- api/User.js
+++ api/User.js
@@ -1,5 +1,5 @@
 const express = require('express');
-router = express.Router();
+const router = express.Router();
 
 // Load User model
 const User = require('../models/User');
@@ -8,55 +8,74 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 // Register a new user
-router.post('/register', (req, res) => {
-    let { email, password, phone_number } = req.body;
+router.post('/register', async (req, res) => {
+    let { email, national_id_number, phone_number, password  } = req.body;
     email = email.trim();
-    password = password.trim();
+    national_id_number = national_id_number.trim();    
     phone_number = phone_number.trim();
+    password = password.trim();
 
-    if (!email || !password || !phone_number) {
-        return res.status(400).json({ message: 'Invalid input' });
+    if (!email || !password || !phone_number || !national_id_number) {
+        return res.status(400).json({ message: 'Invalid input due to blank fields' });
     } else if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
-        return res.status(400).json({ message: 'Invalid email' });
-    } else if (!phone_number.match(/^0\d{9}$/)) {
-        return res.status(400).json({ message: 'Invalid phone number.' });
+        return res.status(400).json({ message: 'Invalid format for email' });
+    } else if (!phone_number.match(/^0\d{9}$/) || !national_id_number.match(/^\d{8}$/)) {
+        return res.status(400).json({ message: 'Invalid format for phone or id number' });
     } else if (!password.match(/^(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$/)) {
         return res.status(400).json({ message: 'Password must be 8 characters long with a special character, number and uppercase letter' });
     } else {
         // Check if user exists
-        User.find({email}).then(result => {
-            if (result.length > 0) {
+        try {
+            const existingUser = await User.findOne({ email })
+            if (existingUser) {
                 return res.status(400).json({ message: 'A user with that email already exists' });
-            } else {
-                // Create a new user
-                const saltRounds = 10;
-                bcrypt.hash(password, saltRounds).then(hashedPassword => {
-                    const newUser = new User({
-                        email,
-                        password: hashedPassword,
-                        phone_number
-                    });
-                    newUser.save().then(result => {
-                        res.status(201).json({ message: 'User created successfully', data: result });
-                    })
-                        .catch(e => {
-                            console.error(e);
-                            res.status(500).json({ message: 'Internal server error while saving user' });
-                        })
-                })
-                    .catch(e => {
-                        console.error(e);
-                        res.status(500).json({ message: 'Internal server error while hashing password' });
-                    })
             }
-        }).catch(e => {
+            // Create a new user document
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            const newUser = new User({
+                email,
+                national_id_number,
+                phone_number,
+                password: hashedPassword
+            })
+            const savedUser = await newUser.save()               
+            res.status(201).json({ message: 'User created successfully', data: savedUser.email });
+               
+        } catch (e) {
             console.error(e);
-            res.status(500).json({ message: 'Internal server error while checking for user' });
-        })
+            res.status(500).json({ message: 'Internal server error while creating user' });
+        }
     }
 })
 
 // Login a user
-// router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    let { national_id_number, password } = req.body;
+    national_id_number = national_id_number.trim();
+    password = password.trim();
 
-// })
+    if (!national_id_number || !password) {
+        return res.status(400).json({ message: 'Empty credentials supplied' });
+    } else {
+        try {
+            const user =  await User.findOne({ national_id_number });
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            } else {
+                const match = await bcrypt.compare(password, user.password);
+                if (match) {
+                    return res.status(200).json({ message: 'User logged in successfully' });
+                } else {
+                    return res.status(401).json({ message: 'Invalid credentials' });
+                }
+            }
+        }
+        catch (e) {
+            console.error(e);
+            res.status(500).json({ message: 'Internal server error while logging in user' });
+        }
+    }
+})
+
+module.exports = router;
