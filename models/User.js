5e6fd5c54d2eda51ce0c54128d2639320104bfd7
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-28 13:36:17 +0300

    Update User model

--- models/User.js
+++ models/User.js
@@ -2,11 +2,48 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    email: {type: String, required: true},
-    national_id_number: {type: String, required: true},
-    phone_number: {type: String, required: true},
-    password: {type: String, required: true},
-});
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    national_id_number: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    phone_number: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 20,
+    },
+    role: {
+        type: String,
+        default: 'license_holder',
+    },
+    refresh_tokens: {
+        type: [String],
+        default: [],
+    },
+    confirmed: {
+        type: Boolean,
+        default: false,
+    },
+    confrimationCode: {
+        type: String,
+        default: '',
+    },
+},
+    { timestamps: true }
+);
 
 const User = mongoose.model('User', UserSchema);
 
