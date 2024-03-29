4723884f8478a2edaa5265fd6f3f73259c244675
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-29 18:13:06 +0300

    Create Payment model

---
+++ models/Payment.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PaymentSchema = new Schema({
+    payment_method: {
+        type: String,
+        required: true,
+    },
+    payment_reference: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    transaction_id: {
+        type: String,
+        required: true,
+    },
+    transaction_date: {
+        type: Date,
+        required: true,
+    },
+    extension_plan: {
+        type: String,
+        required: true,
+        options: ['monthly', 'quarterly', 'bi-annually', 'annually'],
+    },
+    license_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'License',
+    },
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+})
+
+const Payment = mongoose.model('Payment');
+module.exports = Payment;
