a46d09630723118735155af5c5f5fc0c8f17b4bd
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-29 18:15:54 +0300

    Update entry point

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port  = process.env.PORT || 8000;
-const UserRouter = require('./api/User');
+const UserRouter = require('./api/UserRequests');
+const LicenseRouter = require('./api/LicenseRequests');
+const PaymentRouter = require('./api/PaymentRequests');
 const bodyParser = require('body-parser');
 
 const uri = process.env.MONGODB_CONNECTION_STRING;
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/user', UserRouter);
+app.use('/license', LicenseRouter);
+app.use('/payment', PaymentRouter);
 
 async function connect() {
     try {
