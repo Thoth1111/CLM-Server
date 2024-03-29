a63192f68760d10558f3ab4a5ef74217aa229b41
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-29 19:01:25 +0300

    Update auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,10 @@ const jwt = require('jsonwebtoken');
 require ('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-    const authHeader =  req.headers['authorization'];
+    const authHeader =  req.headers['Authorization'];
     const refreshToken = authHeader && authHeader.split(' ')[1];
     if (!refreshToken) return res.status(401).send({message: 'Unauthorized Action'});
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
         if (err) return res.status(403).send({message: 'Invalid token'});
         req.national_id_number = decoded.national_id_number;
         next();
