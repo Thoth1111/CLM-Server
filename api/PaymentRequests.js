e0093deec008a4e8f36d722b933ee19b76d0816d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-01 16:09:04 +0300

    Create basic payment request API file

---
+++ api/PaymentRequests.js
@@ -0,0 +1,49 @@
+const express = require ('express');
+const router = express.Router();
+// const Payment = require('../models/Payment');
+require ('dotenv').config();
+const axios = require('axios');
+const { generatePaymentToken } = require('../middleware/stk');
+
+const SafcomUrl = process.env.SAND_BOX_URL;
+
+router.post('/pay', generatePaymentToken, async (req, res) => {
+    // const amount = req.body.amount;
+    const phoneNumber = req.body.phoneNumber.substring(1);
+    const paymentToken = req.paymentToken;
+    const timeStamp = new Date().toISOString().replace(/[^0-9]/g, "").slice(0, 14);
+    const shortCode = process.env.SHORT_CODE;
+    const passKey = process.env.PASS_KEY;
+    const password = Buffer.from(`${shortCode}${passKey}${timeStamp}`).toString('base64');
+
+    await axios.post(SafcomUrl, 
+        {  
+            "BusinessShortCode": shortCode,   
+            "Password": password,    
+            "Timestamp":timeStamp,    
+            "TransactionType": "CustomerPayBillOnline",    
+            "Amount": "0",    
+            "PartyA":`254${phoneNumber}`,    
+            "PartyB":shortCode,    
+            "PhoneNumber":`254${phoneNumber}`,    
+            "CallBackURL": "https://mydomain.com/path",    
+            "AccountReference":"Test",    
+            "TransactionDesc":"Test Payment"
+        }, 
+        {
+            headers: {
+                Authorization: `Bearer ${paymentToken}`,
+            }
+        }
+    )
+    .then((response) => {
+        console.log(response.data);
+        res.status(200).json({ message: 'Payment request sent successfully', data: response.data });
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(400).json(err.message);
+    });
+})
+         
+module.exports = router;
