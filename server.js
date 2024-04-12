bc04a251227285d0832b63d2366f7a32b9812e20
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-12 20:30:10 +0300

    Update entry point

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const port  = process.env.PORT || 8000;
 const UserRouter = require('./api/UserRequests');
 const LicenseRouter = require('./api/LicenseRequests');
 const PaymentRouter = require('./api/PaymentRequests');
-// const Payment = require('./models/Payment');
-// const License = require('./models/License');
 const bodyParser = require('body-parser');
 const callback = process.env.SAFCOM_STK_CALLBACK_URL;
 
@@ -40,19 +38,15 @@ app.listen(port, ()=> {
 app.post(callback, (req, res) => {
     const callbackData = req.body;
     console.log(callbackData);
-    // if (callbackData.Body.ResultCode !== 0) {
-    //     console.log(callbackData.Body.ResultDesc);
-    // } else {
-    // console.log(callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value, callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value, callbackData.Body.stkCallback.CallbackMetadata.Item[3].Value, callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value);
-    // }
-    return res.json('Ok');
+    if (callbackData.Body.ResultCode !== 0) {
+        console.log(callbackData.Body.ResultDesc);
+    } else {
+    console.log(callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value, callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value, callbackData.Body.stkCallback.CallbackMetadata.Item[3].Value, callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value);
+    }    
     // const payment_method = 'M-Pesa';
     // const transaction_id = callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value;
     // const amount = callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value;
     // const transaction_date = callbackData.Body.stkCallback.CallbackMetadata.Item[3].Value;
     // const phone_number = callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value;
-    // const bunsiness_name = license.business_name;
-    // const business_id = license.business_id;
-    // const license_ref = license._id;
-    // const initiator = license.user_id;    
+    return res.json('Ok');
 });
