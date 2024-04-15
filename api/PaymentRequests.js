12deb244fa32e0171596ba09abffcb2282e8e684
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-15 17:31:44 +0300

    Update Payment Requests API

--- api/PaymentRequests.js
+++ api/PaymentRequests.js
@@ -1,13 +1,33 @@
 const express = require ('express');
 const router = express.Router();
-// const Payment = require('../models/Payment');
+
+// -------------------------For demo purposes-----------------------
+const crypto = require('crypto');
+const generateTransactionID = () => {
+    const buffer = crypto.randomBytes(Math.ceil(10 * 3/4))
+    const base64String = buffer.toString('base64')
+    const alphanumericString = base64String.replace(/[^A-Z0-9]/g, '')
+    const firstTwoLetters = alphanumericString.slice(0, 2).toUpperCase()
+    const remainingChars = alphanumericString.slice(2, 10)
+    return `${firstTwoLetters}${remainingChars}`
+}
+// --------------------------------------------------------------------
+
+const Payment = require('../models/Payment');
+const User = require('../models/User');
 require ('dotenv').config();
 const axios = require('axios');
 const { generatePaymentToken } = require('../middleware/stk');
 const { verifyJWT } = require('../middleware/auth');
+const { updateLicense } = require('../helpers/LicenseUpdater');
 
 router.post('/saf/pay', verifyJWT, generatePaymentToken, async (req, res) => {
-    // const amount = req.body.amount;
+    // -------------------------For demo purposes-----------------------
+    const reqAmount = req.body.amount;
+    const business_name = req.body.business_name;
+    const business_id = req.body.business_id;    
+    // ----------------------------------------------------------------
+    const national_id_number = req.national_id_number;
     const phone_number = req.body.phone_number.substring(1);
     const license_id = req.body.license_id;
     const extension_plan = req.body.extension_plan;
@@ -39,7 +59,22 @@ router.post('/saf/pay', verifyJWT, generatePaymentToken, async (req, res) => {
     )
     .then((response) => {
         console.log(response.data);
-        res.status(200).json({ message: 'Payment request sent successfully', data: response.data });
+        const user = User.findOne({ national_id_number: national_id_number });
+        const receiptDetails = new Payment({
+            payment_method: 'M-Pesa',
+            transaction_id: generateTransactionID(),
+            amount: reqAmount,
+            transaction_date: new Date(),
+            business_name: business_name,
+            business_id: business_id,
+            license_ref: license_id,
+            initiator: user._id,
+            phone_number: phone_number,
+            extension: extension_plan,
+        });
+        receiptDetails.save();
+        updateLicense(license_id, extension_plan);
+        res.status(200).json({ message: 'Payment request sent successfully', data: receiptDetails });
     })
     .catch((error) => {
         console.error(error);
