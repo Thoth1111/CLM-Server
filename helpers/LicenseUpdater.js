bd308ec573369a920d7bac921eace0348ab62fed
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-15 18:57:43 +0300

    Update License Updater

--- helpers/LicenseUpdater.js
+++ helpers/LicenseUpdater.js
@@ -1,34 +1,22 @@
-const License = require('../models/License');
-
-const updateLicense = async (license_id, extensionPlan) => {
-    console.log(license_id, extensionPlan);
-    const license = await License.findOne({ _id: license_id });
-    console.log(license);
-    console.log(`initial expiry: ${license.expiry_date}`);
-    if (!license) return { message: 'License not found' };
+const updateLicense = async (license, extensionPlan) => {
     if (license.expiry_date < new Date()) {
-        console.log(`initial effective date: ${license.effective_date}`);
-        license.effective_date = new Date(license.expiry_date.getTime() + 86400000);
-        console.log(`updated effective date: ${license.effective_date}`);
+      license.effective_date = new Date(license.expiry_date.getTime() + 86400000);
     }
     switch (extensionPlan) {
         case '1 year':
             license.expiry_date.setFullYear(license.expiry_date.getFullYear() + 1);
-            console.log(`updated expiry date: ${license.expiry_date}`);
             break;
         case '6 months':
             license.expiry_date.setMonth(license.expiry_date.getMonth() + 6);
-            console.log(`updated expiry date: ${license.expiry_date}`);
-            break; 
+            break;
         case '3 months':
             license.expiry_date.setMonth(license.expiry_date.getMonth() + 3);
-            console.log(`updated expiry date: ${license.expiry_date}`);
             break;
         default:
             break;
     }
     const updatedLicense = await license.save();
-    return { data: updatedLicense }
+    console.log(updatedLicense);
 }
 
 module.exports = { updateLicense };
