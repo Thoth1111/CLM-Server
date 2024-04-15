bf1890798aeab9af2c81856385de2fdae80b2517
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-15 17:31:57 +0300

    Add License Updater helper

---
+++ helpers/LicenseUpdater.js
@@ -0,0 +1,21 @@
+const License = require('../models/License');
+
+export const updateLicense = async (license_id, extensionPlan) => {
+    const license = await License.findOne({ _id: license_id });
+    if (!license) return { message: 'License not found' };
+    if (license.expiry_date < new Date()) license.effective_date = license.expiry_date + 1;
+    switch (extensionPlan) {
+        case '12 months':
+            license.expiry_date.setFullYear(license.expiry_date.getFullYear() + 1);
+            break;
+        case '6 months':
+            license.expiry_date.setMonth(license.expiry_date.getMonth() + 6);
+            break; 
+        case '3 months':
+            license.expiry_date.setMonth(license.expiry_date.getMonth() + 3);
+            break;
+        default:
+            break;
+    }
+    await license.save();
+}
