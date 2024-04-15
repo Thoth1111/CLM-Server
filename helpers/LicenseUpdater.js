2da8dc0a816c1d451d3dbebece9c8e839bc1ea01
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-16 02:03:14 +0300

    Update License Updater

--- helpers/LicenseUpdater.js
+++ helpers/LicenseUpdater.js
@@ -1,4 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
+import { qrGenerate, deleteQRImage } from './qrGenerator';
+
 const updateLicense = async (license, extensionPlan) => {
+    const qrID = uuidv4();
+    if (license.qr_code_id) {
+        deleteQRImage(license.qr_code_id);
+    }
+    license.qr_code_id = qrID;
     let oldExpiryDate = new Date(license.expiry_date);
     if (oldExpiryDate < new Date()) {
         license.effective_date = new Date(oldExpiryDate.getTime() + 86400000);
@@ -18,6 +26,18 @@ const updateLicense = async (license, extensionPlan) => {
             break;
     }
     license.expiry_date = oldExpiryDate;
+    qrGenerate(
+        qrID, 
+        license.business_name, 
+        oldExpiryDate, 
+        license.location.constituency, 
+        license.location.ward, 
+        license.location.plot_number, 
+        license.location.road_street, 
+        license.location.building, 
+        license.location.floor, 
+        license.location.stall_number
+    )
     const updatedLicense = await license.save();
     return updatedLicense;
 }
